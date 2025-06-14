c3439900ad055aff61377471f6bb2c3d6bc82ea2
ImanPJN <imanpajand@gmail.com>
2025-06-15 03:09:04 +0400

    Delete web3.js

--- web3.js
+++
@@ -1,5 +0,0 @@
-const CONTRACT_ADDRESS = "0xc08279d91abf58a454a5cea8f072b7817409e485";
-const CONTRACT_ABI = [
-  "function submitScore(string memory name, uint256 score) public",
-  "function getTopScores() public view returns (tuple(address player, string name, uint256 score)[])"
-];
