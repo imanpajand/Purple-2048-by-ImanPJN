d38ca5a97ca776bdb40b1c44667a7b03acab5aec
ImanPJN <imanpajand@gmail.com>
2025-07-24 02:23:13 +0400

    Update share.js

--- api/share.js
+++ api/share.js
@@ -1,1 +1,9 @@
 
+export default function handler(req, res) {
+  const text = encodeURIComponent(
+    "Play fun and addictive #2048 game on Base and save your score on-chain on top the leaderboard! 🔵 🎮\nPurple 2048 #MiniApp by @imaan 💜 \nhttps://purple2048.vercel.app"
+  );
+  const url = `https://farcaster.xyz/~/compose?text=${text}`;
+  res.writeHead(302, { Location: url });
+  res.end();
+}
